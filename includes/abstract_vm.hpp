a60615f350b6cf5067bd6f92092f0face9b0ffdb
bsautron <sautron.brunojj@gmail.com>
2016-07-02 23:18:56 +0200

    new

--- includes/abstract_vm.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef ABSTRACT_VM_HPP
-# define ABSTRACT_VM_HPP
-
-# include <iostream>
-# include <Operand.hpp>
-# include <OperandBuilder.hpp>
-
-#endif
--- sources/AbstractVm.cpp
+++
@@ -1,9 +0,0 @@
-#include <AbstractVm.hpp>
-
-AbstractVm::AbstractVm(void) {
-	std::cout << "new AbstractVm" << std::endl;
-}
-
-AbstractVm::~AbstractVm(void) {
-	std::cout << "delete AbstractVm" << std::endl;
-}
